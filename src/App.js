fa66af6c38a95354c85f48b8ed2a403655e24b5c
luckstar77 <luckstar77y@gmail.com>
2021-12-23 16:53:39 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { Input, Button, Card, Col, Row } from 'antd';
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
+import webSocket from 'socket.io-client'
 
 function App() {
+  const [ws,setWs] = useState(null)
+
+    const connectWebSocket = () => {
+        //開啟
+        setWs(webSocket('ws://localhost:3001'))
+    }
+
+    useEffect(()=>{
+        if(ws){
+            //連線成功在 console 中打印訊息
+            console.log('success connect!')
+            //設定監聽
+            initWebSocket()
+        }
+    },[ws])
+
+    const initWebSocket = () => {
+        //對 getMessage 設定監聽，如果 server 有透過 getMessage 傳送訊息，將會在此被捕捉
+        ws.on('getMessage', message => {
+            console.log(message)
+        })
+    }
+
+    const sendMessage = () => {
+        //以 emit 送訊息，並以 getMessage 為名稱送給 server 捕捉
+        ws.emit('getMessage', '只回傳給發送訊息的 client')
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="site-card-wrapper">
+    <Row gutter={16}>
+      <Col span={8} offset={8}>
+        <Card title="井字棋" bordered={false}>
+          <Input.Group compact>
+            <Input addonBefore="玩家名稱"  style={{ width: 'calc(100% - 200px)' }} placeholder="宏宏" />
+            <Input addonBefore="房間名稱"  style={{ width: 'calc(100% - 200px)' }} placeholder="吾疆" />
+            <Button type="primary">加入房間</Button>
+          </Input.Group>
+        </Card>
+      </Col>
+    </Row>
+    <Row gutter={16}>
+      <Col span={8} offset={8}>
+          <Button type="primary" value='連線' onClick={connectWebSocket}>連線</Button>
+      </Col>
+    </Row>
+    <Row gutter={16}>
+      <Col span={8} offset={8}>
+          <Button type="primary" value='送出訊息' onClick={sendMessage}>送出訊息</Button>
+      </Col>
+    </Row>
+  </div>
   );
 }
 
